4d4ee9814afc206fd7eddfd4f230a182407ce3eb
Kip DeMane <kip.demane@gmail.com>
2013-04-02 21:24:13 -0400

    First constants file

---
+++ app/game/api/constants.php
@@ -0,0 +1,10 @@
+<?php
+// stat ID's for easy reference in code
+define('HP',  1);
+define('MP',  2);
+define('Spd', 3);
+define('Atk', 4);
+define('Def', 5);
+define('Mag', 6);
+define('Res', 7);
+?>
